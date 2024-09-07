33d83c40498764eca9f915ae63d322b4419cf8cc
Kevin Dobariya <kevindobariyaa08@gmail.com>
2024-09-07 13:27:16 +0530

    add geolocation and change image type

--- models/listing.js
+++ models/listing.js
@@ -9,14 +9,8 @@ const listingSchema = new mongoose.Schema({
         type:String,
     },
     image:{
-        type:String,
-
-        // defalult is use when image is not declared or undefing
-        default : "https://images.unsplash.com/photo-1494526585095-c41746248156?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        // set is use when image is come but it is empty
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1494526585095-c41746248156?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-
-
+        url:String,
+        filename:String,
     },
     price:{
         type:Number,
@@ -35,6 +29,17 @@ const listingSchema = new mongoose.Schema({
     createdBy: {
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
+    },
+    geometry: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
     },
 });
 
