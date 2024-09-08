a340f5400a388bd209cea9dc0c718002314cb9fd
Kevin Dobariya <kevindobariyaa08@gmail.com>
2024-09-08 09:17:55 +0530

    Add coordinates to every default listing

--- init/index.js
+++ init/index.js
@@ -18,7 +18,10 @@ async function main() {
 
 const initDB = async () =>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, createdBy:"66d74f780c5cec089379e4e4"}));
+    initData.data = initData.data.map((obj) => ({...obj, createdBy:"66d74f780c5cec089379e4e4", geometry: {
+        type: "Point",
+        coordinates: [77.1025, 28.7041]
+    }}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
 }
